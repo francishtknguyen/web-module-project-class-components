f6fccb30802242267c6a3c305fa56165b88e4166
Francis Nguyen <francishtknguyen@gmail.com>
2021-04-05 15:58:26 -0500

    completed mvp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
 import React from 'react';
-
+import TodoForm from './components/TodoForm'
+import TodoList from './components/TodoList'
+const todo = [
+  {
+    task: 'First Task',
+    id: '36912',
+    completed: false
+  }
+]
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(){
+    super();
+    this.state = {
+      todo: todo
+    }
+  }
+
+  toggleItem = (id)=> {
+    const newTodo = this.state.todo.map(task => {
+      if(task.id ===id){
+        return({...task, completed:!task.completed })
+      }
+      else{
+        return task
+      }
+  })
+  this.setState({
+    todo:newTodo
+  })  
+};
+
+  addItem = (itemName)=> {
+    this.setState({
+      todo: [...this.state.todo, {task: itemName, id: this.state.todo.length, completed: false}]
+    })
+  };
+
+  clearItems = ()=> {
+    this.setState({
+      todo: this.state.todo.filter( task => {return !task.completed})
+    })
+  }
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList toggleItem={this.toggleItem} todo={this.state.todo} />
+        <TodoForm addItem = {this.addItem} clearItems={this.clearItems}/>
       </div>
     );
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,17 @@
+import React from 'react'
+import Todo from './Todo'
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+const TodoList = props=> {
+
+    return(
+        <div>
+            {props.todo.map( task => (
+                <Todo toggleItem={props.toggleItem} task ={task} key= {task.id} />
+            ))}
+        </div>
+    )
+}
+
+export default TodoList;
